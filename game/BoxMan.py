a4c2a36af57e00c1d2f77189a26a9a09d71f9316
mayojar <43939932+majormayojar@users.noreply.github.com>
2019-02-16 16:12:25 -0500

    Update BoxMan.py

--- game/BoxMan.py
+++ game/BoxMan.py
@@ -1,19 +1,23 @@
+#Importing stuff
 import pygame
 from pygame.locals import *
 import time
 
+#Initializing pygame and the game
 pygame.init()
 screen = pygame.display.set_mode([1024,768])
 keep_going = True
 pygame.display.set_caption("BoxMan")
 
+#Loading Images
+lvl1 = pygame.image.load("lvl1.png")
+lvl2 = pygame.image.load("lvl2.png")
+lvl3 = pygame.image.load("lvl3.png")
+character = pygame.image.load("character.png")
 
-lvl1 = pygame.image.load("lvl1.bmp")
-lvl2 = pygame.image.load("lvl2.bmp")
-lvl3 = pygame.image.load("lvl3.bmp")
-character = pygame.image.load("character.bmp")
 pygame.display.set_icon(character)
 
+#Variables
 a = 0
 b = 549
 c = 0
@@ -24,8 +28,21 @@ BLACK = (0,0,0)
 x = 1024
 y = 768
 
-
+#Game Loop
 while keep_going:
+    screen.blit(lvl1, (0,0))
+    screen.blit(character,(a,b))
+    if c >= 810:
+        if a >= 810:
+            a = a - a
+        screen.blit(lvl2,(0,0))
+        screen.blit(character,(a,b))
+        pygame.display.update()
+        if a == 470:
+            if b == 549:
+                c = 810
+                a = a - a  
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keep_going = False
@@ -37,28 +54,30 @@ while keep_going:
             a += 10
             c +=10
             if c >= 810:
-                if a == 810:
+                if a >= 810:
                     a = a - a
                 screen.blit(lvl2,(0,0))
-                screen.blit(character,(a,549))
+                screen.blit(character,(a,b))
                 pygame.display.update()
                 if a == 470:
-                    a = a - a
-                    pygame.display.update()
+                    if b == 549:
+                        c = 810
+                        a = a - a
+                        pygame.display.update()
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_r:
                 a = a - a
                 c = c - c
-                screen.blit(character,(a,549))
+                screen.blit(character,(a,b))
                 pygame.display.update()
             if event.key == pygame.K_SPACE:
-                b = 529
+                b += 40
                 screen.blit(character,(a,b))
                 pygame.display.update()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-               b = 549
-               screen.blit(character, (a,b))
+               b -= 40
+               screen.blit(character,(a,b))
                pygame.display.update()
             if event.key == pygame.K_LSHIFT:
                 if c >= 810:
@@ -69,10 +88,12 @@ while keep_going:
                     pygame.display.update()
                 if a == 470:
                     a = a - a
+                    c = 810
                     pygame.display.update()
                 a += 27
                 if a >= 810:
                     a = a - a
                     pygame.display.update()
-    
+
+#Uninitializing pygame    
 pygame.quit()
